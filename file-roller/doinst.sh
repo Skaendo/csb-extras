9fd6a339988ed686aa31795a64fe23937d613ce2
skaendo <skaendo@excite.com>
2017-05-18 07:11:55 -0500

    file-roller: Upgraded to 3.24.1

--- file-roller/doinst.sh
+++ file-roller/doinst.sh
@@ -13,4 +13,6 @@ if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
-glib-compile-schemas /usr/share/glib-2.0/schemas >/dev/null 2>&1
+if [ -x /usr/bin/glib-compile-schemas ]; then
+	/usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas/ 2>/dev/null
+fi
